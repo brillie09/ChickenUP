b9a298fe0c8f7d51b27c7ae5ca8f5b4f8213a364
sangps0909 <sangps0909@gmail.com>
2017-06-06 04:01:40 +0700

    platform update

--- CI/js/main.js
+++ CI/js/main.js
@@ -26,54 +26,49 @@ var preload = function(){
   Nakama.game.load.atlasJSONHash('assets', 'Assets/assets.png', 'Assets/assets.json');
   Nakama.game.load.image('sky', 'assets/OriginalSprites/sky.png');
   Nakama.game.load.image('ground', 'assets/OriginalSprites/platform.png');
-  Nakama.game.load.image('star', 'assets/OriginalSprites/star.png');
   Nakama.game.load.spritesheet('dude', 'assets/OriginalSprites/dude.png', 32, 48);
 }
 
 // initialize the game
 var create = function(){
   Nakama.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+  //background
   Nakama.game.add.sprite(0, 0, 'sky');
+
   //platforms
   platforms = Nakama.game.add.group();
   platforms.enableBody = true;
+
   var ground = platforms.create(0, Nakama.game.world.height - 64, 'ground');
   ground.scale.setTo(2, 2);
   ground.body.immovable = true;
-  var ledge = platforms.create(400, 400, 'ground');
+  var ledge = platforms.create(300, 450, 'ground');
   ledge.body.immovable = true;
-  var ledge = platforms.create(-150, 250, 'ground');
+  ledge.anchor.setTo(0.5, 0.5);
+  var ledge = platforms.create(500, 350, 'ground');
   ledge.body.immovable = true;
+  ledge.anchor.setTo(0.5, 0.5);
+  var ledge = platforms.create(400, 250, 'ground');
+  ledge.body.immovable = true;
+  ledge.anchor.setTo(0.5, 0.5);
+
   //player
   player = Nakama.game.add.sprite(32, Nakama.game.world.height - 150, 'dude');
   Nakama.game.physics.arcade.enable(player);
-  player.body.bounce.y = 0.2;
-  player.body.gravity.y = 300;
+  player.body.bounce.y = 0.1;
+  player.body.gravity.y = 1000;
   player.body.collideWorldBounds = true;
   player.animations.add('left', [0, 1, 2, 3], 10, true);
   player.animations.add('right', [5, 6, 7, 8], 10, true);
   //cursors
   cursors = Nakama.game.input.keyboard.createCursorKeys();
-  //star
-  stars = Nakama.game.add.group();
-  stars.enableBody = true;
-  for(var i = 0; i < 12; i++){
-    var star = stars.create(i * 70, 0, 'star');
-    star.body.gravity.y = 6;
-    star.body.bounce.y = 0.7 + Math.random() * 0.2;
-  }
-
 }
 
 // update game state each frame
 var update = function(){
   //collider
   var hitPlatform = Nakama.game.physics.arcade.collide(player, platforms);
-  Nakama.game.physics.arcade.collide(stars, platforms);
-  Nakama.game.physics.arcade.overlap(player, stars, collectStar, null, this);
-  function collectStar(player, star){
-    star.kill();
-  }
 
   //playerMovement
   player.body.velocity.x = 0;
@@ -90,7 +85,7 @@ var update = function(){
     player.frame = 4;
   }
   if(cursors.up.isDown && player.body.touching.down && hitPlatform){
-    player.body.velocity.y = -350;
+    player.body.velocity.y = -500;
   }
 }
 
